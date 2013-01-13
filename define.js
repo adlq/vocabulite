bba26d3831a61543e3b4b3515a6f5a13a5a4369d
adlq <dtnghia90@gmail.com>
2013-01-13 23:37:52 +0100

    Start fixing visual bug with widget's position and clean up widget's HTML content

--- define.js
+++ define.js
@@ -1,18 +1,41 @@
 (function () {
+	var jQuery;
+	var cssPath = 'http://someday.heliohost.org/vocabulite/vocabulite.css';
+
+	// Widget objects
+	var widget = {};
+	var statusButton = {};
+
+	/*
+	 * Our main Ajax request
+	 */
+	var apiKey = '0f191f643576c490532070f5c310f77af776e4d0abbe3e0c7';
+	var apiUrl = 'http://api.wordnik.com//v4/word.json/';
+	var apiOptions = '/definitions?api_key=' + apiKey;
+
+	var ajaxReq = {
+		url: apiUrl,
+		datatype: 'json',
+		success: function( resp ) {
+			fillWidget( resp );
+		},
+		error: printError
+	};
+
+	/* 
+	 * This object will help us
+	 * identify the highlighted text.
+	 * It contains 2 functions:
+	 * textHighlighter.mouseup()
+	 * textHighlighter.getText()
+	 */
+	var textHighlighter = {};
+
 	/*
 	 * Load jQuery if it's not already present
 	 * Code courtesy of Alex Maradon
 	 * http://alexmarandon.com/articles/web_widget_jquery/
 	 */
-	var jQuery;
-	var apiKey = '0f191f643576c490532070f5c310f77af776e4d0abbe3e0c7';
-	var apiUrl = 'http://api.wordnik.com//v4/word.json/';
-	var apiOptions = '/definitions?limit=5&api_key=' + apiKey;
-	var cssPath = 'vocabulite.css';
-
-	// Widget object
-	var widget = {};
-
 	if ( window.jQuery === undefined || window.jQuery.fn.jquery !== '1.7.2' ) {
 		var scriptTag = document.createElement('script');
 		scriptTag.setAttribute("type", "text/javascript");
@@ -43,79 +66,108 @@
 		main();
 	}
 
+	/* 
+	 * This is called one the jQuery and the
+	 * CSS file have been successfully been
+	 * loaded
+	 */
 	function scriptLoadHandler() {
 		jQuery = window.jQuery.noConflict(true);
 		main();
 	}
 
+	/*
+	 * The main function
+	 */
 	function main() {
 		jQuery( document ).ready( function() {
 
+			// Select all BUT the widget (to bind the mouseup action)
 			var s = jQuery( "body" ).children().not( "#vocabulite_widget" );
 			
-			// If the widget exists, then we clear it
-			var selection = jQuery( '#vocabulite_widget' );
-			if ( selection.length ) {
+			// If the widget exists, then we clear it with the status button, and unbind the actions
+			var existingWidget = jQuery( '#vocabulite_widget' );
+			if ( existingWidget.length ) {
 				s.off( "mouseup.vocabulite" );
+				existingWidget.remove();
+				jQuery( '#vocabulite_status' ).remove();
 			} else {
 				drawWidget();
+				s.on( "mouseup.vocabulite", event, textHighlighter.mouseup );
 			}
-
-			console.log('Ready!');
-			s.on( "mouseup.vocabulite", event, textHighlighter.mouseup );
 		});
 	}
 
+	/*
+	 * This function will draw the widget
+	 * then hide it
+	 */
 	var drawWidget = function() {
 
 		// Draw the widget itself, if it doesn't exist yet
 		jQuery( 'body' ).append( "<div id='vocabulite_widget'></div>" );
 		widget.w = jQuery( '#vocabulite_widget' );
-		widget.w.hide();
 
-		// Outer measures take into account the padding to correctly position the widget
-		widget.realHeight = widget.w.outerHeight();
-		widget.realWidth = widget.w.outerWidth();
-	}
+		/*
+		 * Outer measures take into account the padding to correctly position the widget
+		 * We currently have to do it this way because outerHeight() and outerWidth() on
+		 * hidden elements is unstable
+		 */
+		if ( widget.w.length ) {
+			// There are still some visual bug with this
+			// that might come from the hidden status of 
+			// the widget, we could have used jQuery functions
+			// hide() and show() but we chose to implement them 
+			// by changing CSS property "visibility" because 
+			// apparently it causes less bug (?)
+			widget.padding = parseInt( widget.w.css( 'padding-top' ).replace( 'px', '' ), 10 );
+			widget.outerHeight = widget.w.height() + widget.padding;
+			widget.outerWidth = widget.w.width() + widget.padding;
 
-	var textHighlighter = {};
-
-	/* 
-	 * Get value of highlighted text
-	 * Code courtesy of Jeff Anderson
-	 * http://www.codetoad.com/javascript_get_selected_text.asp
-	 */
-	textHighlighter.getText = function() {
-		var text = '';
-
-		if( window.getSelection ) {
-			text = window.getSelection();
-		} else if ( document.getSelection ) {
-			text = document.getSelection();
-		} else if ( document.selection ) {
-			text = document.selection.createRange().text;
+			hideWidget();
 		}
-		return text;
+
+		// Draw status button
+		jQuery( 'body' ).append( "<div id='vocabulite_status'>V</div>" );
+		statusButton.b = jQuery( '#vocabulite_status' );
 	}
 
+	/*
+	 * Function to call when mouseup.vocabulite event
+	 * is triggered. This basically positions the widget,
+	 * fill it and shows it.
+	 */
 	textHighlighter.mouseup = function( event ) {
 		var string = preprocessString( textHighlighter.getText().toString() );
 
 		if ( relevantString( string ) ) {
-			console.log("Selected : " + string);
+			//console.log("Selected : " + string);
 			/* Toggle definition widget */
 
 			/*
 			 * Position the widget, such as there's no
 			 * unecessary scrolling to reveal it.
 			 */
-			
 			var scrollTop = jQuery( document ).scrollTop();
 			var scrollLeft = jQuery( document ).scrollLeft();
-			widget.x = ( window.innerWidth + scrollLeft - event.pageX - widget.realWidth < 0 ?
-					Math.max( event.pageX - widget.realWidth, scrollLeft ) : event.pageX );
-			widget.y = ( window.innerHeight + scrollTop - event.pageY - widget.realHeight < 0 ? 
-					Math.max( event.pageY - widget.realHeight, scrollTop ) : event.pageY );
+
+			/*
+			console.log( "innerHeight = " + window.innerHeight );
+			console.log( "scrollTop = " + scrollTop );
+			console.log( "eventY = " + event.pageY );
+			console.log( "wouterHeight = " + widget.outerHeight );
+			console.log( "wouterWidth = " + widget.outerWidth );
+			*/
+			
+			widget.x = ( window.innerWidth + scrollLeft - event.pageX - widget.outerWidth < 0 ?
+					Math.max( event.pageX - widget.outerWidth, scrollLeft ) : event.pageX );
+			widget.y = ( window.innerHeight + scrollTop - event.pageY - widget.outerHeight < 0 ? 
+					Math.max( event.pageY - widget.outerHeight, scrollTop ) : event.pageY );
+			/*
+			console.log( "wx = " + widget.x );
+			console.log( "wy = " + widget.y );
+			*/
+			
 			widget.w.css( {
 				'left': widget.x + 'px',
 				'top': widget.y + 'px',
@@ -126,27 +178,34 @@
 			jQuery.ajax(ajaxReq);
 
 			// Show the widget
-			widget.w.show();
+			showWidget();
 		} else {
 			// Set the widget content to blank
 			widget.w.empty();
 
 			// Hide the widget
-			widget.w.hide();
+			hideWidget();
 		}
 	}
 
 	/*
-	 * Our main Ajax request
+	 * Hide widget
 	 */
-	var ajaxReq = {
-		url: apiUrl,
-		datatype: 'json',
-		success: function( resp ) {
-			fillWidget( resp );
-		},
-		error: printError
-	};
+	var hideWidget = function() {
+		if ( widget.w.length ) {
+			widget.w.css( { 'visibility': 'hidden' } );
+		}
+	}
+
+	/*
+	 * Show widget
+	 */
+	var showWidget = function() {
+		if ( widget.w.length ) {
+			widget.w.css( { 'visibility': 'visible' } );
+		}
+	}
+
 
 	/* 
 	 * Preprocess highlighted string
@@ -158,7 +217,7 @@
 		string = jQuery.trim( string );
 		string = string.toLowerCase();
 
-		console.log( "Used in ajax request: " + string );
+		//console.log( "Used in ajax request: " + string );
 
 		return string;
 
@@ -185,24 +244,84 @@
 	 */
 	var fillWidget = function( resp ) {
 
+		/*
+		 * Global object containing the widget's content
+		 * widget.content = {
+		 * 	partOfSpeech1 : [
+		 * 		{ def1, src1 }
+		 * 		{ def2, src2 }
+		 * 		{ def3, src3 }
+		 * 	]
+		 * 	partOfSpeech2 : [
+		 * 		{ def1, src1 }
+		 * 	]...
+		 * }
+		 */
+		widget.content = {};
+
+		// If the response is not empty
 		if ( resp.length ) {
+			// Print the word as a title!
+			widget.w.append( "<h1 class='vocab_title'>" + resp[0].word + "</h1>" );
+
+			/*
+			 * We store each result with respect to
+			 * the part of speech (noun, adjective, ...)
+			 * in order to have a somewhat better layout
+			 */
 			for ( var i = 0; i < resp.length; i++) {
-				widget.w.html( formatContent( resp[i] ) );
+				
+				/*
+				 * For each part of speech, insert it as 
+				 * object property if not already present.
+				 * Otherwise, store definition and sourceDictionary
+				 * in it
+				 */
+				var POS = resp[i].partOfSpeech.toString();
+				if ( !( POS in widget.content ) ) {
+					widget.content[POS] = [];
+				}
+				widget.content[POS].push( { 
+					def: resp[i].text, 
+					src: resp[i].sourceDictionary 
+				} );
 			}
+
+			/*
+			 * Finally, call the formatContent()
+			 * function to render the appropriate 
+			 * HTML code
+			 */
+			widget.w.append( formatContent() );
 		} else {
-			widget.w.html( "No definition found" );
+			widget.w.html( "No definition found!" );
 		}
 	}
 
 	/*
-	 * Takes a JSON object and return some 
-	 * HTML to fill into the widget
+	 * Render the appropriate HTML code
+	 * given that widget.content has been filled
 	 */
-	var formatContent = function( resp ) {
+	var formatContent = function() {
 		var content = '';
 
-		content += "<h1>" + resp.word + "</h1>";
-		content += "<p>" + resp.text + "</p>";
+		/*
+		 * Iterate through all of widget.content's 
+		 * properties, which are parts of speech
+		 */
+		for ( var POS in widget.content ) {
+			content += '<h2>' + POS + '</h2>';
+			content += '<ul>';
+			// Iterate through each definition
+			for ( var i = 0; i < widget.content[POS].length; i++ ) {
+				content += '<li>' + widget.content[POS][i]['def'] 
+					+ "<span style='background: #000; color: #fff'>(" 
+					+ widget.content[POS][i]['src']
+					+ ')</span>'
+					+ '</li>';
+			}
+			content += '</ul>';
+		}
 
 		return content;
 	}
@@ -224,4 +343,22 @@
 		return ( string != '' && string.match( "[\w]*" ) );
 	}
 	
+	/* 
+	 * Get value of highlighted text
+	 * Code courtesy of Jeff Anderson
+	 * http://www.codetoad.com/javascript_get_selected_text.asp
+	 */
+	textHighlighter.getText = function() {
+		var text = '';
+
+		if( window.getSelection ) {
+			text = window.getSelection();
+		} else if ( document.getSelection ) {
+			text = document.getSelection();
+		} else if ( document.selection ) {
+			text = document.selection.createRange().text;
+		}
+		return text;
+	}
+
 })();
